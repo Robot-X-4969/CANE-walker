251144d038b21065bf628e8e6846bc9e44685499
Evan Bretl <bretle8545@stxavier.org>
2016-02-20 16:39:05 -0500

    us_test is a short program that prints the values seen by each ultrasonic sensor on the walker

---
+++ US_Test/US_Test.ino
@@ -0,0 +1,33 @@
+int trigger = 30;
+
+void setup() {
+  pinMode(trigger, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+    for(int echo = 22; echo <= 27; echo++) {
+      int data = microsToCm( ping( echo ));
+      Serial.print( data ); Serial.print("\t");
+    }
+    Serial.println();
+    delay(500); 
+}
+
+
+
+long ping(int echo) {
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(4);
+  digitalWrite(trigger, LOW);
+  long time = pulseIn(echo, HIGH, 50000);
+  return time;
+}
+
+long microsToInches(long t){
+  return t / 73.5 / 2;
+}
+long microsToCm(long t){
+  return t / 29 / 2;
+}
+
