f6bb8ada8b9b53cb843dc8f10fa799e83d1eb143
Evan Bretl <evanbretl4@gmail.com>
2016-06-26 14:01:41 -0400

    changed sound player from time to clock

--- src/sound.py
+++ src/sound.py
@@ -1,24 +1,24 @@
 from threading import Thread
 from os import system
-from time import time, sleep
+from time import clock, sleep
 
 class SoxSoundThread (Thread):
     def __init__(self, filename):
         Thread.__init__(self)
         self.cmd_str = 'play -q ' + filename
-        self.t_last = time()
-        self.delay = 1000000
+        self.t_last = clock()
+        self.delay = 1000000.0
         self.is_robotting = True
 
     def run(self):
         while self.is_robotting:
-            if time() > self.t_last + self.delay:
+            if clock() > self.t_last + self.delay:
                 #print('running', self.cmd_str)
-                self.t_last = time()
+                self.t_last = clock()
                 system( self.cmd_str )
                 #print('finished', self.cmd_str)
             else:
-                sleep(0.02)
+                sleep(0.05)
 
     def set_frequency(self, frequency):
         self.delay = 1.0 / frequency
