85f59fac4e659892301efc19d5e4bf07b0767a18
Evan Bretl <evanbretl4@gmail.com>
2016-07-28 20:28:33 -0400

    Improved readability of ultrasonic module

--- src/ultrasonic.py
+++ src/ultrasonic.py
@@ -2,23 +2,25 @@ import RPi.GPIO as gpio
 from time import clock
 from threading import Thread
 
+# distance (in meters, technically) to use when no object is seen. This 
+# should be outside the range in which the walker reacts in any way
+REALLY_FAR_AWAY = 49.69
 
-REALLY_FAR_AWAY = 1000
-
-class UltrasonicSensor:
-    """ 
-    Constructor Parameters:
-	* trigger_pin - number for the GPIO pin connected to TRIG on the sensor
-	* echo_pin - number for the GPIO pin connected to ECHO on the sensor
-	* offset - distance from sensor when walker hits an obstacle
-		^ can be zero
-	* max_dist - ignores objects farther than this (after offset applied)
-      * min_blip_freq - beep tempo at farthest distance
-      * max_blip_freq - beep tempo at nearest distance
-    """
+class UltrasonicSensor: 
+    #Constructor Parameters:
+    # * trigger_pin - number for the GPIO pin connected to TRIG on the sensor
+    # * echo_pin - number for the GPIO pin connected to ECHO on the sensor
+    # * offset - distance from sensor when walker hits an obstacle
+    # 	^ can be zero
+    # * max_dist - ignores objects farther than this (after offset applied)
+    #  * min_blip_freq - beep tempo at farthest distance
+    # * max_blip_freq - beep tempo at nearest distance
+    #Other ariables stored:
+    # * distance - most recent measurement (in meters)
+    # * timeout - time after which CANE should stop listening for US echo
     
-
-    def __init__(self, trigger_pin, echo_pin, offset, max_dist, min_blip_freq, max_blip_freq):
+    def __init__(self, trigger_pin, echo_pin, offset, max_dist, 
+                 min_blip_freq, max_blip_freq):
         self.echo = echo_pin
         self.trigger = trigger_pin
         self.distance = 0  # should be >= 0
@@ -26,7 +28,7 @@ class UltrasonicSensor:
         self.dist_max = max_dist
         self.max_freq = max_blip_freq
         self.min_freq = min_blip_freq
-        self.timeout = meters_to_seconds(6.0)
+        self.timeout = meters_to_seconds(6.0) #upper range 1m-4.5m, depending
         gpio.setmode(gpio.BCM)
         gpio.setup(self.trigger, gpio.OUT)
         gpio.setup(self.echo, gpio.IN)
@@ -36,34 +38,40 @@ class UltrasonicSensor:
 
     def check_echo_ended(self):
         return not gpio.input(self.echo)
-
+        
+    # send pulse to sensor's TRIG pin, starting the measurement process
     def ping(self):
         gpio.output(self.trigger, gpio.LOW)
         micros_wait(10)
         gpio.output(self.trigger, gpio.HIGH)
         micros_wait(10)
         gpio.output(self.trigger, gpio.LOW)
-
+    
+    # update the sensor's distance variable (see resources directory for 
+    # technical info)
     def find_distance(self):
+        # wait until the echo pin goes high, ignoring how long it takes
         time_check(self.check_echo_started, self.timeout)
-        tmp_time = time_check(self.check_echo_ended, self.timeout)
-        raw_distance = seconds_to_meters( tmp_time )
-        self.distance = raw_distance - self.dist_offset
-        #print("""'went high after', wait_time, """'measured time', tmp_time)
-        print('raw distance', self.distance)
-        if raw_distance < 0: #timed out
+        # wait until the echo pin goes low, storing the elapsed time
+        echo_time = time_check(self.check_echo_ended, self.timeout)
+        if echo_time < 0: #timed out; there is nothing in view
             self.distance = REALLY_FAR_AWAY
-        elif self.distance < 0.0: #found something but it's too close
-            self.distance = 0.0  #thus self.distance >= 0 always
-        return
+        else:
+            #convert to meters and adjust for offset
+            self.distance = seconds_to_meters(echo_time) - self.dist_offset
+            if self.distance < 0.0: 
+                #found something but it's less than the minimum distance
+                self.distance = 0.0
             
-
+    # return a new thread which runs find_distance()
     def get_distance_thread(self):
         return Thread(target=UltrasonicSensor.find_distance, args=(self,))
-
+    
+    # get the freqency at which the UI beeps should occur for this sensor
     def blips_freq(self):
         if self.distance < self.dist_max:
-            return self.max_freq - (self.max_freq - self.min_freq) / self.dist_max * self.distance
+            return (self.max_freq - (self.max_freq - self.min_freq) 
+                    / self.dist_max * self.distance)
         else:
             return 0.001
 
@@ -76,12 +84,17 @@ def seconds_to_meters(seconds):
 def meters_to_seconds(meters):
 	return meters / 170.145
 
+# time.sleep() only works with input > 1 ms. This is inelegnt, but works with
+# the full accuracy of the operating system. Only use when time.sleep()
+# would be insufficient
 def micros_wait(t):
     tEnd = clock() + t * 10**-6
     while clock() < tEnd:
         pass
     return
 
+# run a function repeatedly until it returns true, then return the time
+# it took to reach that state. If max_time (in seconds) is 
 def time_check( return_checker, max_time ):
     tStart = clock()
     tTimeout = tStart + max_time
