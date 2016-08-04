2288b4aec35c73f531698084da5dda653dc8132b
Evan Bretl <evanbretl4@gmail.com>
2016-08-04 10:21:26 -0400

    switched to right side sensors

--- cane_main.py
+++ cane_main.py
@@ -25,17 +25,17 @@ BLIP_FREQ_MAX = 5.0
 
 #TODO fix pin locations
 sensors = [
-    UltrasonicSensor(TRIG_L, ECHO_LS, OFFSET_SIDE, MAX_DIST_SIDE, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
-    UltrasonicSensor(TRIG_L, ECHO_LF, OFFSET_FRONT, MAX_DIST_FRONT, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
-    #UltrasonicSensor(TRIG_R, ECHO_RF, OFFSET_FRONT, MAX_DIST_FRONT, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
-    #UltrasonicSensor(TRIG_R, ECHO_RS, OFFSET_SIDE, MAX_DIST_SIDE, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
+    #UltrasonicSensor(TRIG_L, ECHO_LS, OFFSET_SIDE, MAX_DIST_SIDE, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
+    #UltrasonicSensor(TRIG_L, ECHO_LF, OFFSET_FRONT, MAX_DIST_FRONT, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
+    UltrasonicSensor(TRIG_R, ECHO_RF, OFFSET_FRONT, MAX_DIST_FRONT, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
+    UltrasonicSensor(TRIG_R, ECHO_RS, OFFSET_SIDE, MAX_DIST_SIDE, BLIP_FREQ_MIN, BLIP_FREQ_MAX),
 ]
 
 sound_repeaters = [
-    SoxSoundThread(SOUND_LS),
-    SoxSoundThread(SOUND_LF),
-    #SoxSoundThread(SOUND_RF),
-    #SoxSoundThread(SOUND_RS),
+    #SoxSoundThread(SOUND_LS),
+    #SoxSoundThread(SOUND_LF),
+    SoxSoundThread(SOUND_RF),
+    SoxSoundThread(SOUND_RS),
 ]
 
 for sr in sound_repeaters:
