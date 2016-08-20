da3180d852c585d8d78db1b5e46da45075c5a9f8
Nicholas Reichert <joe13reich@gmail.com>
2016-08-20 14:37:29 -0400

    Fixed lots of bugs with the newUltrasonic sensor code.

--- src/newUltrasonic.py
+++ src/newUltrasonic.py
@@ -1,4 +1,4 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
 import time
 import threading
 
@@ -7,14 +7,17 @@ class UltrasonicThread (threading.Thread):
     distanceOptions = None # Distance options object, used to hold values for processing.
 
     def __init__(self, triggerPins, echoPins):
+        self.stateMachines = [] # Initialize stateMachines as a list.
         for trigger, echo in zip(triggerPins, echoPins):
-            self.stateMachiens.add(UltrasonicStateMachine(trigger, echo))
+            self.stateMachines.append(UltrasonicStateMachine(trigger, echo))
         self.distanceOptions = DistanceOptions()
+        threading.Thread.__init__(self)
 
     def run(self):
-        for machine in self.stateMachines:
-            machine.update()
-        time.sleep(0.00001) # TODO Adjust this value.
+        while True:
+            for machine in self.stateMachines:
+                machine.update()
+            time.sleep(0.00001) # TODO Adjust this value.
 
 class DistanceOptions:
     # Content
@@ -35,8 +38,8 @@ class UltrasonicStateMachine:
         self.setUpGPIO()
 
     def setUpGPIO(self):
-        GPIO.setmode(gpio.BCM)
-        if triggerPin is not None:
+        GPIO.setmode(GPIO.BCM)
+        if self.triggerPin is not None:
            GPIO.setup(self.triggerPin, GPIO.OUT)
         GPIO.setup(self.echoPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -53,33 +56,40 @@ class UltrasonicStateMachine:
             self.calculateRawTime()
     
     def startTriggerPing(self):
-        GPIO.output(self.triggerPin, GPIO.HIGH)
-        state += 1
+        if self.triggerPin is not None:
+            GPIO.output(self.triggerPin, GPIO.HIGH)
+        self.state += 1
     
     triggerEndTime = 0
     waitingForTrigger = False
     def waitForTriggerEnd(self):
-        if self.waitingForTrigger == False:
-            self.waitingForTrigger = True
-            self.triggerEndTime = time.clock() + (10 * 10**-6) # Stop in 10 microseconds.
-        elif self.triggerEndTime < time.clock():
-            self.waitingForTrigger = False
+        if self.triggerPin is None:
             self.state += 1
+        else:
+            if self.waitingForTrigger == False:
+                self.waitingForTrigger = True
+                self.triggerEndTime = time.clock() + (10 * 10**-6) # Stop in 10 microseconds.
+            elif self.triggerEndTime < time.clock():
+                if self.triggerPin is not None:
+                    GPIO.output(self.triggerPin, GPIO.LOW)
+                self.waitingForTrigger = False
+                self.state += 1
     
     echoStartTime = 0
     def waitForEchoStart(self):
-        if GPIO.input(echoPin) == GPIO.HIGH:
+        if GPIO.input(self.echoPin) == GPIO.HIGH:
             self.echoStartTime = time.clock()
             self.state += 1
 
     echoEndTime = 0
     def waitForEchoEnd(self):
-        if GPIO.input(echoPin) == GPIO.LOW:
+        if GPIO.input(self.echoPin) == GPIO.LOW:
             self.echoEndTime = time.clock()
             self.state += 1
 
     def calculateRawTime(self):
         self.rawTime = self.echoEndTime - self.echoStartTime
-        print(self.rawTime)
+        print("Pin: " + str(self.echoPin) + " Time: " + str(self.rawTime))
+        self.state = 0
 
 
